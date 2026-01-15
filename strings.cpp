908ed70abf8c45deb65a76f905e8d100843a94fc
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-15 14:10:29 +0530

    commiting after sometime

--- strings.cpp
+++ strings.cpp
@@ -4,5 +4,9 @@ using namespace std;
 int main(){
     string a = "arka";
     cout << a[1] << endl; // will print out r
+    // find size of a string
+    cout << a.size()<< endl; // prints out teh total number of characters in a and the index
+    a[a.size()-1]= 'm'; // replaces the last a in arka and makes it arkm, also this is just a character so single quotes
+    cout << a<< endl;
     return 0;
 }
