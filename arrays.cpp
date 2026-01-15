2648db48b0df45a1ffcfb3fc840c3ae1e1a46eb2
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-15 12:37:37 +0530

    commiting after sometime

--- arrays.cpp
+++ arrays.cpp
@@ -1,1 +1,11 @@
 // when do we use arrays
+# include <iostream>
+using namespace std;
+
+int main(){
+    int arr(5);
+
+    return 0;
+}
+// array elements are always stored next to each other in the memory but where the
+// first element will go is unknown
