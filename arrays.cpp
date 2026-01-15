1428fb8d3153e02ea79254b9718883639cb2a8c8
Arka Banerjee <124756974+thearkabanerjee@users.noreply.github.com>
2026-01-15 13:50:39 +0530

    commiting after sometime

--- arrays.cpp
+++ arrays.cpp
@@ -2,10 +2,18 @@
 # include <iostream>
 using namespace std;
 
-int main(){
-    int arr(5);
-
-    return 0;
-}
+// int main(){
+//     int arr[3];
+//     cin >> arr[0]>> arr[1] >> arr[2];
+//     cout << arr;
+//     return 0;
+// }
 // array elements are always stored next to each other in the memory but where the
 // first element will go is unknown
+
+int main(){
+    int array[3]= {1, 2, 3};
+    cout << array<< endl;
+// this doesnt cout the array thou. it probably gives out the memory location or something similar.
+    return 0;
+}
